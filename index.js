a2c47848ce74066c8916f4df7055ecadcf187607
amineennaciri <spitlight.enn@gmail.com>
2023-03-23 12:09:55 +0000

    in index.js the binarysearchTree function works nicely as it was confirmed using the prettyprint() function that prints the whole tree.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
-function Node(val){
-    this.val = val
+function Node(data){
+    this.data = data
     this.left = null
     this.right = null
 }
 //Build a Tree class / factory which accepts an array when initialized. The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
 function Tree(arr){
-    this.root = buildTree()
+    this.root = buildTree(arr)
 }
 //Write a buildTree function which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
 function buildTree(arr){
@@ -20,13 +20,15 @@ function buildTree(arr){
             }
         }
     }
-    console.log(arr);//before sorting
-    console.log(arr.length);
+    //console.log(arr);//before sorting
+    //console.log(arr.length);
     // sort array
     // Sort the array in ascending order
     arr.sort((a, b) => a - b);
-    console.log(arr.length);
+    //console.log(arr.length);
     // now that the array is sorted let's turn the array into a balanced binary tree
+    console.log(arr);
+    console.log(arr.length);
     let lowInterval = 0;//start
     let highInterval = arr.length-1;//end
     return sortedArrayToBST(arr,lowInterval,highInterval);
@@ -40,18 +42,22 @@ function sortedArrayToBST(arr,start,end){
         return null;
     }
     /* Get the middle element and make it root */
-    let middle = Math.floor((start+end)/2);
-    let node = new Node(arr[middle]);
+    const middle = Math.floor((start+end)/2);
+    //const middle = parseInt((start+end)/2);
+    const root = new Node(arr[middle]);
+    //console.log(root);
     /* Recursively construct the left subtree and make it
     left child of root */
-    node.left = sortedArrayToBST(arrm,start,middle-1);
+    root.left = sortedArrayToBST(arr,start,middle-1);
+    //console.log(root);
     /* Recursively construct the right subtree and make it
     right child of root */
-    node.right = sortedArrayToBST(arr, middle + 1, end);
-    return node;
+    root.right = sortedArrayToBST(arr, middle + 1, end);
+    //console.log(root);
+    return root;
 }
 // binary search
-function binarySearch(arr,target){
+/* function binarySearch(arr,target){
     let lowInterval = 0;
     let highInterval = arr.length-1;
     while(lowInterval<=highInterval){
@@ -68,12 +74,29 @@ function binarySearch(arr,target){
         }
     }
     return null;
+} */
+//If you would like to visualize your binary search tree, here is a prettyPrint()
+//This function will expect to receive the root of your tree as the value for the node parameter.
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
 }
 
-
-
 let data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-console.log(buildTree(data));
+let dataTest = [50,25,75,10,33,56,89,4,11,30,40,52,61,82,95]
+// [1,3,4,5,7,8,9,23,67,324,6345]//length = 11
+console.log(buildTree(dataTest));
 //console.log(`let's try binary search tree`);
 //console.log(binarySearch([3,17,75,80,202],22));
 //console.log(binarySearch([3,17,75,80,202],75));
+prettyPrint(buildTree(dataTest));
+//prettyPrint(buildTree(arr),prefix = '', isLeft = true);
+//console.log(binaryTree(data));
